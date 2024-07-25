c3316e58310591ce111d187204576ff7f079b233
BrianMath <brianmatheus10@gmail.com>
2024-07-25 11:42:58 -0300

    Aula 25/07/2024

---
+++ lab14_compilacaoLigacaoCodigoMaquina/aux.s
@@ -0,0 +1,66 @@
+	.file	"aux.c"
+	.text
+	.data
+	.align 4
+	.type	b, @object
+	.size	b, 4
+b:
+	.long	10
+	.text
+	.globl	fill_a
+	.type	fill_a, @function
+fill_a:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movq	%rdi, -8(%rbp)
+	movq	-8(%rbp), %rax
+	movl	$100, (%rax)
+	nop
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	fill_a, .-fill_a
+	.globl	get_b
+	.type	get_b, @function
+get_b:
+.LFB1:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	b(%rip), %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	get_b, .-get_b
+	.ident	"GCC: (Ubuntu 13.2.0-23ubuntu4) 13.2.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:
